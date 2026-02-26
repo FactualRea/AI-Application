5897eb2911b488d7b001638235d3449ef160fb9f
Reatlegile <rmatsepe30@gmail.com>
2026-02-26 13:06:15 +0200

    main edit

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ api_key = os.getenv("OPENAI_API_KEY")
 if not api_key:
     raise ValueError("OPENAI_API_KEY not found in env file")
 
+#print("Loaded key:", api_key[:4] + "…")
+
 def get_response(prompt):
     client = OpenAI(api_key = api_key)
     response = client.chat.completions.create(
@@ -15,5 +17,5 @@ def get_response(prompt):
     )
     return response.choices[0].message.content
 
-response = get_response("explain about different role,  user, system and use bullet point for your response")
+response = get_response("explain about different role, user, system and use bullet point for your response")
 print(response)
